4d718c6a75ffca09deb1af33b066163ab78f3a8f
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-24 01:47:22 +0300

    working on add_user

--- flask_sql/app.py
+++ flask_sql/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
-
 # Configure MySQL connection
 db_config = {
     'user': 'root',
@@ -26,7 +25,7 @@ def index():
     connection.close()
     return render_template('index.html', users=users)
 
-@app.route('/<fname>')
+@app.route('/get_user <fname>', methods=['GET'])
 def get_user(fname):
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
@@ -38,7 +37,32 @@ def get_user(fname):
     if user:
         return jsonify(user)
     else:
-        return 'user not found', 404 
+        return 'user not found', 404
+    
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    id = request.form.get('id')
+    img_url = request.form.get('img_url')
+    register_date = request.form.get('register_date')
+    if not fname or not lname or not id or not img_url or not register_date:
+        return jsonify({"error": "Invalid or missing data"}), 400
+    connection = get_db_connection()
+    if connection is None:
+        return "Database connection failed", 500
+    cursor = connection.cursor()
+    cursor.execute = ('''INSERT INTO users 
+                    (fname, lname, id, img_url, register_date)
+                    VALUES (%s, %s,%s,%s,%s)'''
+                    , (fname, lname, id, img_url, register_date))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return redirect(url_for('index1'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
