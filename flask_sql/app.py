17f49bd27dfe4fad9449d54fe71ada320a53d831
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-24 19:30:45 +0300

    add user & update the database

--- flask_sql/app.py
+++ flask_sql/app.py
@@ -23,7 +23,7 @@ def index():
     users = cursor.fetchall()
     cursor.close()
     connection.close()
-    return render_template('index.html', users=users)
+    return jsonify(users)
 
 @app.route('/get_user <fname>', methods=['GET'])
 def get_user(fname):
@@ -39,20 +39,24 @@ def get_user(fname):
     else:
         return 'user not found', 404
     
-@app.route('/add_user', methods=['POST'])
+@app.route('/add_user')
 def add_user():
-    fname = request.form.get('fname')
-    lname = request.form.get('lname')
-    id = request.form.get('id')
-    img_url = request.form.get('img_url')
-    register_date = request.form.get('register_date')
+    return render_template('index1.html')
+
+@app.route('/add_user', methods=['POST'])
+def create_user():
+    fname = request.form['fname']
+    lname = request.form['lname']
+    id = request.form['id']
+    img_url = request.form['img_url']
+    register_date = request.form['register_date']
     if not fname or not lname or not id or not img_url or not register_date:
         return jsonify({"error": "Invalid or missing data"}), 400
     connection = get_db_connection()
     if connection is None:
         return "Database connection failed", 500
     cursor = connection.cursor()
-    cursor.execute = ('''INSERT INTO users 
+    cursor.execute ('''INSERT INTO users 
                     (fname, lname, id, img_url, register_date)
                     VALUES (%s, %s,%s,%s,%s)'''
                     , (fname, lname, id, img_url, register_date))
@@ -60,7 +64,7 @@ def add_user():
     cursor.close()
     connection.close()
 
-    return redirect(url_for('index1'))
+    return redirect('/')
 
 
 if __name__ == '__main__':
