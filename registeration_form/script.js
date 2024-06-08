497f0b3e2525cf6ac6e91dc3eeeebeed0f774c43
jailan-sh <jilan.elsherbiny@gmail.com>
2024-06-09 00:51:40 +0300

    up

--- registeration_form/script.js
+++ registeration_form/script.js
@@ -1,2 +1,11 @@
 "use strict";
 
+// selecting element
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const contact = document.getElementById("mobile");
+const gender = document.getElementById("gender");
+const registerBtn = document.querySelector(".registerbtn");
+
+// collecting input data
